6361c6772eb4235cc96bc50dad05db477c86453c
lucky13men <sergey200.87@mail.ru>
2022-10-18 00:11:29 +0300

    add game_v3.py edit readme.md

---
+++ project_0/game_v3.py
@@ -0,0 +1,62 @@
+"""Игра угадай число.
+Компьютер сам загадывает и угадывает число
+"""
+import numpy as np
+
+def random_predict(number:int=1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. По умолчанию кмопьютер
+        выбирает случайное число в диапазоне от 1 до 100.
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    Returns:
+        int: Число попыток
+    """
+
+    count = 0
+    min_pr_num = 1
+    max_pr_num = 100
+    predict_number = np.random.randint(min_pr_num, max_pr_num)
+
+    while True:
+        count += 1
+        if number > predict_number:
+            min_pr_num = predict_number
+        elif number < predict_number:
+            max_pr_num = predict_number
+        else:
+            break # выход из цикла, если угадали
+    return(count)
+
+print(f'Количество попыток: {random_predict()}')
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш
+    алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости на другом 
+    random_array = np.random.randint(1, 101, size=(1000)) # заг-ли список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+# RUN
+#if __name__ == '__main__':      # программа запустится, если файл будет
+#    score_game(random_predict)  # исполняемым, но не импортируемым (для работы
+                                # функции нужно вызвать её после импорта файла)
+print(score_game(random_predict))
